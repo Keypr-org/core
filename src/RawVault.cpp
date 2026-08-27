efcae491822ad8dd360632a1490d39338d4a254e
Nolan <nolan.evard@heig-vd.ch>
2026-08-27 11:14:51 +0200

    [Feat]: Add raw vault parsing and corresponding tests

--- src/RawVault.cpp
+++ src/RawVault.cpp
@@ -1,8 +1,39 @@
 #include "RawVault.h"
+#include "VaultHeader.h"
 
-// RawVault RawVault::parse(Bytes data) {
-//     // TODO: Implement this
-//     return RawVault(VaultHeader(std::array<uint8_t, 8>(), 0, std::array<uint8_t, 32>(), 0, 0),
-//                     std::array<uint8_t, 32>(), std::array<uint8_t, 24>(), std::array<uint8_t,
-//                     16>(), std::vector<uint8_t>());
-// }
+RawVault RawVault::parse(Bytes data) {
+    if (data.size() < VAULT_FILE_MIN_SIZE) {
+        throw RawVaultParsingError("Data is too small to be a valid vault file.");
+    }
+
+    // Parse header
+    // It must be initialized with a lambda as VaultHeader is not default-constructible
+    // Help received from ChatGPT to figure this out
+    VaultHeader header = [&] {
+        try {
+            return VaultHeader::parse(data);
+        } catch (const VaultHeaderParsingError& e) {
+            throw RawVaultParsingError("Failed to parse vault header: " + std::string(e.what()));
+        }
+    }();
+
+    // Parse header MAC
+    std::array<uint8_t, 32> headerMAC;
+    std::copy(data.begin() + HEADER_MAC_OFFSET, data.begin() + HEADER_MAC_OFFSET + HEADER_MAC_BYTES,
+              headerMAC.begin());
+
+    // Parse XSalsa20 nonce
+    std::array<uint8_t, 24> xSalsa20Nonce;
+    std::copy(data.begin() + XSALSA20_NONCE_OFFSET,
+              data.begin() + XSALSA20_NONCE_OFFSET + XSALSA20_NONCE_BYTES, xSalsa20Nonce.begin());
+
+    // Parse ciphertext MAC
+    std::array<uint8_t, 16> ciphertextMAC;
+    std::copy(data.begin() + CIPHERTEXT_MAC_OFFSET,
+              data.begin() + CIPHERTEXT_MAC_OFFSET + CIPHERTEXT_MAC_BYTES, ciphertextMAC.begin());
+
+    // Parse ciphertext
+    std::vector<uint8_t> ciphertext(data.begin() + CIPHERTEXT_OFFSET, data.end());
+
+    return RawVault(header, headerMAC, xSalsa20Nonce, ciphertextMAC, ciphertext);
+}
