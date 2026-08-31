3f728d4754c2f7e0d87ca71afd6258e38bebe7bb
Nolan <nolan.evard@heig-vd.ch>
2026-08-31 12:01:07 +0200

    [Feat]: Add VaultRepository vaultExists method implementation

---
+++ include/VaultRepository.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+
+class VaultRepository {
+  public:
+    VaultRepository() = default;
+
+    /*
+     * @brief Checks if a vault file exists and is valid.
+     *
+     * @param filename The name of the vault file to check.
+     *
+     * @return true if the vault file exists and is valid, false otherwise.
+     */
+    bool vaultExists(std::string filename);
+};
---
+++ src/VaultRepository.cpp
@@ -0,0 +1,16 @@
+#include "VaultRepository.h"
+#include "FileHandler.h"
+#include "RawVault.h"
+
+bool VaultRepository::vaultExists(std::string filename) {
+    if (!FileHandler::fileExists(filename))
+        return false;
+    try {
+        RawVault::parse(FileHandler::readFile(filename));
+    } catch (RawVaultParsingError& e) {
+        return false;
+    } catch (FileHandlerError& e) {
+        return false;
+    }
+    return true;
+}
